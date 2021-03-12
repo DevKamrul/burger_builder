6f5f093e5964f10929f38d816ca88048cfcd066b
Kamrul Hasan <kamrul.dev@gmail.com>
2021-03-12 17:44:09 +0600

    navbar creation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import Logo from '../../Assets/burger.png';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
+        <div className="Navigation">
+            <Navbar style={{
+                background: "#D70f64",
+                Height: "70px"
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand"> <img src={Logo} alt="logo" width="100px" /> </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#" className="NavLink">Home</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
