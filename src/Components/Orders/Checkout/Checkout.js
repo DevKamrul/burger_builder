584c9c14bb62b56eb8f65847b59da07a7bec5546
Kamrul Hasan <kamrul.dev@gmail.com>
2021-03-27 15:58:20 +0600

    Add axios and use firebase for checkout data

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+const mapStateToProps = (state) => {
+    return {
+        ingredient: state.ingredient,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
 
 class Checkout extends Component {
     state = {
         values: {
+            name: " ",
             deliveryAddress: " ",
             phone: " ",
             paymentType: "Cash on delivery",
@@ -24,12 +35,28 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values)
+        const order = {
+            ingredient: this.props.ingredient,
+            Price: this.props.totalPrice,
+            customer: this.state.values,
+            orderTime: new Date(),
+        }
+        axios.post("https://burger-builder-app-cbe43-default-rtdb.firebaseio.com/orders.json", order)
+            .then(Response => console.log(Response))
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <div>
+                <h4 style={
+                    {
+                        border: "1px solid grey",
+                        boxShadow: "1px 1px #888888",
+                        borderRadius: "5px",
+                        padding: "20px"
+                    }
+                }>Total Price : {this.props.totalPrice} BDT</h4>
                 <form style={
                     {
                         border: "1px solid grey",
@@ -38,6 +65,9 @@ class Checkout extends Component {
                         padding: "20px"
                     }
                 }>
+                    <div class="form-group">
+                        <input onChange={(e) => this.inputChangerHandler(e)} name="name" value={this.state.values.name} className="form-control" placeholder="Your Name" />
+                    </div>
                     <div class="form-group">
                         <textarea onChange={(e) => this.inputChangerHandler(e)} name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Delivery Address"></textarea>
                     </div>
@@ -58,7 +88,7 @@ class Checkout extends Component {
     }
 }
 
-export default Checkout;
+export default connect(mapStateToProps)(Checkout);
 
 
 
