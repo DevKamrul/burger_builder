36f28f3b7d387f13c728ab2b55ea8c19fe2799e3
Kamrul Hasan <kamrul.dev@gmail.com>
2021-04-17 17:23:57 +0600

    Form validation added

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -18,13 +18,46 @@ export default class Auth extends Component {
                             console.log(values)
                         }
                     }
+                    validate={(values) => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = "Required";
+                        }
+                        // else if () {
+                        //     errors.email = "Invalid Email Address"
+                        // }
+                        if (!values.password) {
+                            errors.password = "Required";
+                        }
+                        else if (values.password.length < 8) {
+                            errors.password = "Min 8 Carecter required ";
+                        }
+                        if (!values.passwordConfirm) {
+                            errors.passwordConfirm = "Required";
+                        }
+                        else if (values.password !== values.passwordConfirm) {
+                            errors.passwordConfirm = "Password Filed doesn't match"
+                        }
+                        return errors;
+                    }}
                 >
-                    {({ values, handleChange, handleSubmit }) => (
-                        <div>
+                    {({ values, handleChange, handleSubmit, errors }) => (
+                        <div style={{
+                            border: "1px solid black",
+                            padding: "15px",
+                            borderRadius: "7px"
+
+                        }}>
                             <form onSubmit={handleSubmit}>
-                                <input name="email" placeholder="Your Email" className="form-control" value={values.email} onChange={handleChange} /><br />
-                                <input name="password" placeholder="Your Password" className="form-control" value={values.password} onChange={handleChange} /><br />
-                                <input name="passwordConfirm" placeholder="Confirm Password" className="form-control" value={values.passwordConfirm} onChange={handleChange} /><br />
+                                <input name="email" placeholder="Your Email" className="form-control" value={values.email} onChange={handleChange} />
+                                <span style={{ color: "red" }}>{errors.email}</span>
+                                <br />
+                                <input name="password" placeholder="Your Password" className="form-control" value={values.password} onChange={handleChange} />
+                                <span style={{ color: "red" }}>{errors.password}</span>
+                                <br />
+                                <input name="passwordConfirm" placeholder="Confirm Password" className="form-control" value={values.passwordConfirm} onChange={handleChange} />
+                                <span style={{ color: "red" }}>{errors.passwordConfirm}</span>
+                                <br />
                                 <button type="submit" className="btn btn-success">Sign Up</button>
                             </form>
                         </div>
